9e48552b3e1a006f58f8f6f5efe81cd15f35041d
ZanderHo <freedom_0003@qq.com>
2015-09-10 14:31:30 +0800

    Initial Commit

---
+++ HzPlayingViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HzPlayingViewDemo
+//
+//  Created by ZanderHo on 15/9/10.
+//  Copyright (c) 2015年 ZanderHo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
